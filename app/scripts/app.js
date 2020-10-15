b0285bb554f086dabfd214c502976672ce6b8671
Rabbi50 <jasrabbi50@gmail.com>
2020-10-15 16:56:27 +0600

    complete first lesson

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,11 +19,11 @@ Instructions:
    * @param {String} response - The unparsed JSON response from get.
    */
   function addSearchHeader(response) {
-    try {
-      response = JSON.parse(response).query;  // you'll be moving this line out of here in the next quiz!
-    } catch (e) {
-      // it's 'unknown', so leave it alone
-    }
+    // try {
+    //   response = JSON.parse(response).query;  // you'll be moving this line out of here in the next quiz!
+    // } catch (e) {
+    //   // it's 'unknown', so leave it alone
+    // }
     home.innerHTML = '<h2 class="page-title">query: ' + response + '</h2>';
   }
 
@@ -36,22 +36,33 @@ Instructions:
     /*
     This code needs to get wrapped in a Promise!
      */
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.onload = function() {
-      if (req.status === 200) {
-        // It worked!
-        // You'll want to resolve with the data from req.response
-      } else {
-        // It failed :(
-        // Be nice and reject with req.statusText
-      }
-    };
-    req.onerror = function() {
-      // It failed :(
-      // Pass a 'Network Error' to reject
-    };
-    req.send();
+    // return new Promise(function(resolve,reject){
+    //   var req = new XMLHttpRequest();
+    //   req.open('GET', url);
+    //   req.onload = function() {
+    //     if (req.status === 200) {
+    //       // It worked!
+    //       resolve(req.response);
+    //     } else {
+    //       // It failed :(
+    //       // Be nice and reject with req.statusText
+    //       reject(req.statusText);
+    //     }
+    //   };
+    //   req.onerror = function() {
+    //     // It failed :(
+    //     // Pass a 'Network Error' to reject
+    //     reject('Network Error');
+    //   };
+    //   req.send();
+    // });
+    return fetch(url);
+  }
+
+  function getJson(url){
+    return get(url).then(function(response){
+      return response.json();
+    })
   }
 
   window.addEventListener('WebComponentsReady', function() {
@@ -61,6 +72,18 @@ Instructions:
     You'll need to add a .then and a .catch. Pass the response to addSearchHeader on resolve or
     pass 'unknown' to addSearchHeader if it rejects.
      */
-    // get('../data/earth-like-results.json')
+    //http://udacity.github.io/exoplanet-explorer/site/app
+    getJson('../data/earth-like-results.json').then(function(response){
+      console.log(response);
+      addSearchHeader(response.query);
+      return response.results[0];
+    })
+    .then(function(url){
+      console.log(url);
+    })
+    .catch(function(error){
+      addSearchHeader('unknown');
+      console.log(error);
+    });
   });
 })(document);
